b08b552064b403823fb76a61693bad7d66f7c847
ldayanamercado <ldayanamercado@unicesar.edu.co>
2023-10-31 16:40:37 -0500

    remote state

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  cloud {
+    organization = "ldayanamercado"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,14 +18,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
-  }
 }
